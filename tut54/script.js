b176e37edeb015f425d6b02ee941c2d9b8498847
DevRahul17 <rahulkumar906960@gmail.com>
2026-07-26 01:01:58 +0530

    tut54 added

---
+++ tut54/script.js
@@ -0,0 +1,20 @@
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking good...")
+
+var a = prompt("Enter your number")
+
+var isTrue = confirm("Are you leaving this page")
+if(isTrue){
+    console.log("Leaving the page")
+}
+
+else{
+    console.log("Not leaving the page")
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
