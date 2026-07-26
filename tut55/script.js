e8a97764f6f590966e27e726aa35ba5ca98c69ff
DevRahul17 <rahulkumar906960@gmail.com>
2026-07-26 23:50:41 +0530

    tut55 added

---
+++ tut55/script.js
@@ -0,0 +1,43 @@
+console.log("Hey this is Tutorial 55");
+
+var a = 5;
+var d = 6;
+
+let b = 6;
+let g = 8;
+
+//console.log(a + d);
+// console.log(typeof a, typeof b);
+// console.log(typeof b + typeof g);
+
+{
+    //let a = 66;
+    console.log(a);
+}
+
+console.log(a);
+
+//const a1 = 6;
+//a1 = a1 + 1;//Not allowed because a1 is constant
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
