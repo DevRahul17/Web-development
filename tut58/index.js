ae1271d27f5a27c4b576233d2e4d453ce94b7b69
DevRahul17 <rahulkumar906960@gmail.com>
2026-07-30 23:28:43 +0530

    tut58 added

---
+++ tut58/index.js
@@ -0,0 +1,8 @@
+function nice(name) {
+    console.log("Hey " + name + " you are nice!")
+    console.log("Hey " + name + " you are good!")
+    console.log("Hey " + name + " your tshirt is nice!")
+    console.log("Hey " + name + " your course is good too!")
+}
+nice("Rahul");
+nice("Harry");
