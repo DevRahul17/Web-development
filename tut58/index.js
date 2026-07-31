fe36c9230257a9fe4e240e48fd2435141606afd9
DevRahul17 <rahulkumar906960@gmail.com>
2026-07-31 19:08:22 +0530

    tut58 completed

--- tut58/index.js
+++ tut58/index.js
@@ -6,3 +6,21 @@ function nice(name) {
 }
 nice("Rahul");
 nice("Harry");
+
+function sum(a,b, c = 3){//c is optional and default parameter
+    //console.log(a+b);
+    return a + b + c;
+}
+
+result = sum(5,7);
+result1 = sum(1,2,5);
+console.log("The sum of a and b is = ",result);
+console.log("The sum of a and b is = ",result1);
+
+const func1 = (x)=>{
+    console.log("I am an arrow function ", x);
+}
+
+func1(5);
+func1(15);
+func1(52);
