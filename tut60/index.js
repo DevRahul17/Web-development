fa32a5e96ff59af04c49d59518c9a2a0d424b12a
DevRahul17 <rahulkumar906960@gmail.com>
2026-08-01 17:17:28 +0530

    tut60 added

---
+++ tut60/index.js
@@ -0,0 +1,33 @@
+console.log("It is String Tutorial");
+let a = "Rahul";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+//console.log(a[5]);//Undefined
+
+console.log(a.length);
+
+let myname = "Rahul";
+let friend = "Prithvi";
+
+console.log("My name is " + myname + " and my friend name is " + friend );
+console.log(`My name is ${myname} and my friend name is ${friend}`);
+//Template Literals (Backticks `)
+
+let b = "Codewithharry";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1,5));
+console.log(b.slice(0));
+
+console.log(b.replace("ha","ca"));
+console.log(b.concat(a,"Tanya","Sohail"));
+console.log(b);
+
+console.log(b.startsWith("Co"));
+console.log(b.startsWith("c"));
+console.log(b.endsWith("arry"));
