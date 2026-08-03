035a68713863f40417209feaf013a9682eb26d64
DevRahul17 <rahulkumar906960@gmail.com>
2026-08-04 00:39:28 +0530

    tut68 added

---
+++ tut68/script.js
@@ -0,0 +1,15 @@
+console.log("Javascript");
+
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
+
+boxes[2].style.backgroundColor = "red";
+
+document.getElementById("redbox").style.backgroundColor = "purple";
+
+document.querySelector(".box").style.backgroundColor = "orange";
+console.log(document.querySelectorAll(".box"));~
+
+// document.querySelectorAll(".box").forEach((e) => {
+//     e.style.backgroundColor = "hotpink";
+// })
