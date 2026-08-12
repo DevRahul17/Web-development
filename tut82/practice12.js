e1f5995a7b8dfcf5d15e971a034d7f477770259f
DevRahul17 <rahulkumar906960@gmail.com>
2026-08-12 23:04:45 +0530

    practice12 js added

---
+++ tut82/practice12.js
@@ -0,0 +1,20 @@
+// Q1.Write a Javascript program to print Hello World after 2 second delay.
+// setTimeout(() => {
+//     console.log("Hello World");
+// }, 2000);
+
+// Q2.Write a Javascript program to find average of numbers in an array using spread syntax.
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// let average = (...nums) => {
+//     let sum = nums.reduce((total, num) => total + num, 0);
+//     return sum / nums.length;
+// }
+
+// console.log("Average =",average(...numbers));
+
+// ... in average(...numbers) → spread
+// ... in (...nums) → rest
+
+// Q3. Write a Javascript function which resolves a promise after n seconds.The function takes n as the parameter.Use an IIFE to execute the functions with different values of n.
