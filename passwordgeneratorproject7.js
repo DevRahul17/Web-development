aa710dbbdb0a36a9eca535ca20c1b24883a3124d
DevRahul17 <rahulkumar906960@gmail.com>
2026-08-14 23:13:24 +0530

    exercise added

---
+++ passwordgeneratorproject7.js
@@ -0,0 +1,5 @@
+// Exercise 7 - Password Generator
+// Create a javscript program capable of generating a password which contains atleast one lowercase,one uppercase and one special characters.Create a password class to acheive the Same.
+
+// Exercise 8 - Alarm Clock
+// The HTML AudioElement Interface can be used to play audio in the browser.Create an alarm clock which displays time and plays sound at a user specified time.
