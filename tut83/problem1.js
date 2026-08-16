641b3fbe346e69097b9b2004927aa716e12afe16
DevRahul17 <rahulkumar906960@gmail.com>
2026-08-16 23:51:33 +0530

    problem1 added

---
+++ tut83/problem1.js
@@ -0,0 +1,27 @@
+
+
+function houseassign(students) {
+    let houses = [];
+
+    for (const student of students) {
+        let house;
+        if (student.length < 6) {
+            house = "Blue";
+        }
+        else if (student.length < 8) {
+            house = "Green";
+        }
+        else if (student.length < 12) {
+            house = "Red";
+        }
+        else {
+            house = "Yellow";
+        }
+        houses.push(`${student} - ${house}`);
+    }
+    return houses;
+}
+
+let students = ["Rahul", "Shanealia", "Dharmanshu", "Aditya", "Abhi", "Faizan", "Mehraan", "Harsh", "Yash", "Vikas", "Kirti", "Janvi", "Bhumika", "Siddharth", "Manual", "Manish", "glennphilips", "shikhardhawan", "shubmangill"];
+
+console.log(houseassign(students));
