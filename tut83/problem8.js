6eb3d0e747b2b0b1ff8d45b878e5cfbb6ec8a8fa
Rahul Kumar <rahulkumar906960@gmail.com>
2026-08-19 16:36:36 +0530

    Add order and filter functions with examples
    Added functions for placing an order and ordering coffee, including random delays. Implemented a product filtering function to filter products by category.

--- tut83/problem8.js
+++ tut83/problem8.js
@@ -15,3 +15,63 @@ const promises = await doubleNumbers([1, 2, 3, 4]);
 
 console.log(await Promise.all(promises));
 // [2, 4, 6, 8]
+
+//Problem 9
+async function placeorder() {
+    return new Promise((resolve) => {
+        //Randomly delay between 1 to 3 seconds
+        const delay = Math.floor(Math.random() * 2000) + 1000;
+
+        setTimeout(() => {
+            resolve("Order placed successfully! Your order is confirmed.");
+        }, delay);
+    });
+}
+
+//Calling the function
+placeorder().then((message) => {
+    console.log(message);
+})
+
+// How it works
+// async function placeOrder() → defines an asynchronous function.
+// new Promise() → creates a promise.
+// Math.random() → generates a random delay.
+// setTimeout() → waits for that random amount of time.
+// resolve() → fulfills the promise with the confirmation message.
+// .then() → receives the resolved message.
+
+//Problem 10
+//Using async/await
+async function brecoffee(coffeetype) {
+    return new Promise((resolve) => {
+        const delay = Math.floor(Math.random() * 2000) + 1000;
+
+        setTimeout(() => {
+            resolve(`${coffeetype} is ready!`);
+        }, delay)
+    });
+}
+
+async function ordercoffee() {
+    const result = await brecoffee("Blackcoffee");
+    console.log(result);
+}
+
+ordercoffee();
+
+function filterProducts(products, criterion) {
+    return products.filter(product => product.category === criterion);
+}
+
+// Example
+const products = [
+    { name: "Laptop", category: "Electronics" },
+    { name: "Shirt", category: "Clothing" },
+    { name: "Phone", category: "Electronics" },
+    { name: "Shoes", category: "Footwear" }
+];
+
+const result = filterProducts(products, "Electronics");
+
+console.log(result);
