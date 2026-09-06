51c4907eae744434bdf59822ea6412d79bb312b0
DevRahul17 <rahulkumar906960@gmail.com>
2026-09-06 23:50:24 +0530

    tut86 added

---
+++ tut86/main1.js
@@ -0,0 +1,14 @@
+//named export
+// import {a, b} from "./mymodule.js"
+// console.log(a,b);
+
+//default export
+// import obj from "./mymodule.js"
+// console.log(obj);
+//Same
+// import rahul from "./mymodule.js"
+// console.log(rahul);
+
+//Required Commonjs
+const a = require("./mymodule2.js");//For import
+console.log(a);
---
+++ tut86/mymodule.js
@@ -0,0 +1,13 @@
+export const a = 1;//named named export
+export const b = 2;//named named export
+export const c = 3;//named named export
+export const d = 4;//named named export
+export const e = 5;//named named export
+
+//Default export
+//It can be accesed by any other name than obj
+let obj;
+export default obj = {
+    x: 5,
+    y: 7
+}
